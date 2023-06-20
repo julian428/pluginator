1532ed05e39f6a3f9394fed0a02a8d6ee4a1ebb6
Julian Metzger <julianjometzger@gmail.com>
2023-06-20 23:26:05 +0200

    started the project again

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,22 @@ module.exports = {
 	theme: {
 		extend: {},
 	},
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					"primary": "#F1DAC4",
+					"secondary": "#48A9A6",
+					"accent": "#474973",
+					"neutral": "#161B33",
+					"base-100": "#0D0C1D",
+					"info": "#3ABFF8",
+					"success": "#36D399",
+					"warning": "#FBBD23",
+					"error": "#F87272",
+				},
+			},
+		],
+	},
 	plugins: [require("daisyui")],
 }
